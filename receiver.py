80abefbbc0473235cad8943dfb680895c2b9242b
Patiljyoti12 <59950788+Patiljyoti12@users.noreply.github.com>
2022-04-13 16:35:59 +0530

    Update receiver.py

--- receiver.py
+++ receiver.py
@@ -1,1 +1,14 @@
+import sender
 
+def get_minimum_temperature_sample(temperature_list):
+    return min(temperature_list)
+    
+def get_maximum_temperature_sample(temperature_list):
+    return max(temperature_list)
+    
+def get_minimum_current_sample(current_sample_list):
+    return min(current_sample_list)
+    
+def get_maximum_current_sample(current_sample_list):
+    return max(current_sample_list)
+  
