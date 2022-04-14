796eb0410673252b2686b036cc695b0f68247bc0
Patiljyoti12 <59950788+Patiljyoti12@users.noreply.github.com>
2022-04-14 14:05:07 +0530

    Update receiver_test.py

--- receiver_test.py
+++ receiver_test.py
@@ -1,1 +1,24 @@
+import unittest
+import receiver
+
+
+class receiver_test(unittest.TestCase):
+
+    def test_minimum_temperature_sample(self)
+        self.assertTrue(receiver.get_minimum_temperature_sample([109.4,73.4,102.2,158.0,96.8])==73.4)
+    
+    def test_maximum_temperature_sample(self):
+        self.assertTrue(receiver.get_maximum_temperature_sample([109.4,73.4,102.2,158.0,96.8])==158.0)
+    
+    def test_minimum_current_sample(self):
+        self.assertTrue(receiver.get_minimum_current_sample([5,6,6,4,1])==1 )
+    
+    def test_maximum_current_sample(self):
+        self.assertTrue(receiver.get_maximum_current_sample([8,9,5,1])== 9)
+  
+    
+    
+if __name__ == '__main__':
+  unittest.main()
+  
 
