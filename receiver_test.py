5f3a4e9f33c610cf7e4ad01188065438f32b3b10
Patiljyoti12 <59950788+Patiljyoti12@users.noreply.github.com>
2022-04-20 23:21:31 +0530

    Update receiver_test.py

--- receiver_test.py
+++ receiver_test.py
@@ -15,6 +15,9 @@ class receiver_test(unittest.TestCase):
     
     def test_maximum_current_sample(self):
         self.assertTrue(receiver.get_maximum_current_sample([8,9,5,1])== 9)
+     
+    def test_current_movingAveragevalue(self):
+        self.assertTrue(receiver.current_movingAveragevalue([40,30,55,50,60],3)==[41.67, 45.0, 55.0])
   
     
     
