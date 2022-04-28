1cbf100c66297e06123004b325dd53deaeca2332
Patiljyoti12 <59950788+Patiljyoti12@users.noreply.github.com>
2022-04-28 12:21:28 +0530

    Update receiver.py

--- receiver.py
+++ receiver.py
@@ -13,6 +13,7 @@ def process_data_from_sender(data):
     data_list = data.split("\n")
     amps_list = []
     temp_list = []
+    data_list = [i for i in data_list if i]
     for value in data_list:
         json_data = json.loads(value)
         amps_list.append(json_data['apms'])
