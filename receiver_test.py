54c16713a99b4e5831b96115fa18289be2c89689
Patiljyoti12 <59950788+Patiljyoti12@users.noreply.github.com>
2022-04-28 10:28:27 +0530

    Update receiver_test.py

--- receiver_test.py
+++ receiver_test.py
@@ -31,7 +31,7 @@ class receiver_test(unittest.TestCase):
          self.assertTrue(receiver.compute_statistics([4, 6, 3, 9, 10, 0],[100.4, 131.0, 138.2, 167.0, 167.0, 123.8])==(0 ,10, 100.4, 167.0, [6.4, 5.6],[140.72, 145.4]))
         
     def test_process_data_from_sender(self):
-        self.assertTrue(receiver.process_data_from_sender(data)==(0 ,10, 100.4, 167.0, [6.4, 5.6],[140.72, 145.4]))   
+        self.assertTrue(receiver.process_data_from_sender(data,6)==(0 ,10, 100.4, 167.0, [6.4, 5.6],[140.72, 145.4]))   
        
         
     
